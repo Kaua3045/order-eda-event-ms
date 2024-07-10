3f8f6063db0ed952c77ac28fe85ceaacc0b1a76f
Kauã Pereira <zkauacodes@hotmail.com>
2024-07-10 16:14:37 -0300

    feat(event-store): add the load events using aggregate id and return domain events list

--- application/src/main/java/com/kaua/order/application/repositories/EventStore.java
+++ application/src/main/java/com/kaua/order/application/repositories/EventStore.java
@@ -1,8 +1,13 @@
 package com.kaua.order.application.repositories;
 
 import com.kaua.order.domain.AggregateRoot;
+import com.kaua.order.domain.events.DomainEvent;
+
+import java.util.List;
 
 public interface EventStore {
 
     <T extends AggregateRoot<?>> void save(T aggregateRoot);
+
+    <T extends DomainEvent> List<T> loadEvents(String aggregateId);
 }
