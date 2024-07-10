6879cfe913891852c0821bd2eb04b728ae94769f
Kauã Pereira <zkauacodes@hotmail.com>
2024-07-10 16:18:15 -0300

    feat(events-jpa-repository): add the find by aggregate id method

--- infrastructure/src/main/java/com/kaua/order/infrastructure/events/persistence/EventsJpaRepository.java
+++ infrastructure/src/main/java/com/kaua/order/infrastructure/events/persistence/EventsJpaRepository.java
@@ -2,5 +2,9 @@ package com.kaua.order.infrastructure.events.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventsJpaRepository extends JpaRepository<EventsJpaEntity, String> {
+
+    List<EventsJpaEntity> findByAggregateId(String aggregateId);
 }
